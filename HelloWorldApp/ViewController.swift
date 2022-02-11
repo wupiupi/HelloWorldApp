04a5cff9cfc7776c15728251e7df58d03d076dd5
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-11 23:07:30 +0300

    Made the interface

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLbl: UILabel!
+    @IBOutlet weak var showTextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showTextBtnPressed() {
+    }
+    
 }
 
