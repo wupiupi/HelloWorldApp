c89910b85b6d2c2f5075cb4259b477b4394ae75b
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-12 19:45:38 +0300

    Complete!

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var helloWorldLbl: UILabel!
-    @IBOutlet weak var showTextBtn: UIButton!
+    @IBOutlet weak var showTextBtn: UIButton! //Для кнопки создана лишь потому, что мы хотим поработать с её внешним видом
+    @IBOutlet weak var aboutAppText: UIButton!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() //Главное -- писать после этой строки :)
         // Do any additional setup after loading the view.
+//        helloWorldLbl.textColor = .blue   //Можно поменять цвет
+        helloWorldLbl.isHidden = true //Скрываем текст на момент запуска приложения. Доступно для любого аутлета
+        showTextBtn.layer.cornerRadius = 5.5 //Скругляем углы кнопки на 10
+        aboutAppText.layer.cornerRadius = 5.5
     }
     
     @IBAction func showTextBtnPressed() {
+        if helloWorldLbl.isHidden {
+            helloWorldLbl.isHidden = false
+            showTextBtn.setTitle("Hide text!", for: .normal) //Меняем название кнопки в нормальном состоянии на "спрятать текст"
+        } else {
+            helloWorldLbl.isHidden = true
+            showTextBtn.setTitle("Show text!", for: .normal)
+        }
     }
     
 }
